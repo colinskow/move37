0053feacf9e6aa0d5bfe53d742c0e23764756a7e
Colin Skow <colinskow@gmail.com>
2018-11-04 18:20:22 -0800

    add a2c continuous

--- actor_critic/lib/common.py
+++ actor_critic/lib/common.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 import numpy as np
+import ptan
 
 import torch
 import torch.nn as nn
@@ -82,6 +83,40 @@ def unpack_batch(batch, net, last_val_gamma, device='cpu'):
     # these are the Q(s,a) values we will use to calculate the advantage and value loss
     q_vals_v = torch.FloatTensor(rewards_np).to(device)
     return states_v, actions_t, q_vals_v
+
+
+def unpack_batch_continuous(batch, net, last_val_gamma, device="cpu"):
+    """
+    Convert batch into training tensors
+    :param batch:
+    :param net:
+    :return: states variable, actions tensor, reference values variable
+    """
+    states = []
+    actions = []
+    rewards = []
+    not_done_idx = []
+    last_states = []
+    for idx, exp in enumerate(batch):
+        states.append(exp.state)
+        actions.append(exp.action)
+        rewards.append(exp.reward)
+        if exp.last_state is not None:
+            not_done_idx.append(idx)
+            last_states.append(exp.last_state)
+    states_v = ptan.agent.float32_preprocessor(states).to(device)
+    actions_v = torch.FloatTensor(actions).to(device)
+
+    # handle rewards
+    rewards_np = np.array(rewards, dtype=np.float32)
+    if not_done_idx:
+        last_states_v = ptan.agent.float32_preprocessor(last_states).to(device)
+        last_vals_v = net(last_states_v)[2]
+        last_vals_np = last_vals_v.data.cpu().numpy()[:, 0]
+        rewards_np[not_done_idx] += last_val_gamma * last_vals_np
+
+    ref_vals_v = torch.FloatTensor(rewards_np).to(device)
+    return states_v, actions_v, ref_vals_v
 
 
 class RewardTracker:
